f0afcde105066c85b1e40ca719e5096276c72d13
Barna Nagy <barna@meetmarkko.com>
2024-11-28 11:21:01 +0000

    add eslint, prettier, husky

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    // domains: ['https://code23-stag-marketplace-public.s3.eu-west-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'code23-stag-marketplace-public.s3.eu-west-2.amazonaws.com',
+        port: '',
+        pathname: '/partydip.test/**',
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
