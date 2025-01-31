44e6c7e232d38d50ca699b249b166e87d6d69e2a
Barna Nagy <barna@meetmarkko.com>
2025-01-31 16:10:55 +0000

    add sessions

---
+++ src/lib/session.ts
@@ -0,0 +1,23 @@
+import { SessionOptions } from 'iron-session'
+
+export interface SessionData {
+  oauth?: {
+    token_type: string
+    expires_in: number
+    access_token: string
+    refresh_token: string
+    scope: string
+  }
+  isLoggedIn: boolean
+  email: string
+}
+
+export const sessionOptions: SessionOptions = {
+  password:
+    process.env.SESSION_SECRET ||
+    'complex_password_at_least_32_characters_long',
+  cookieName: 'markko_session',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+}
